6b89e767eb9d204021ae26fd33230fc0e73611e6
wakouboy <wakouboy@gmail.com>
2017-01-22 11:47:52 +0800

    'storyline'

--- _server/server.js
+++ _server/server.js
@@ -1,22 +1,27 @@
 /**
  * Created by huangxinxin on 2017/1/18.
  */
-var express = require('express')
-var csv2json = require('csv2json')
-var fs = require('fs')
-var server = express()
-var aqiBeijingJson = require('./data/aqi-beijing.json')
-// var populationJson = require('./data/population.json')
+let express = require('express')
+let csv2json = require('csv2json')
+let fs = require('fs')
+let server = express()
+let teamCompeteInfoJson = require('../assets/data/teaminfo.json')
+let teamColor = require('../assets/data/teamcolor.json')
 
-// var populationData = null;
+server.get('/api/get_team_compete_info', function (req, res) {
+  res.json(teamCompeteInfoJson)
+})
+server.get('/api/get_team_color', function (req, res) {
+  res.json(teamColor)
+})
+// let populationJson = require('./data/population.json')
+
+// let populationData = null;
 // fs.createReadStream('./data/population.csv')
 //   .pipe(csv2json({
 //     separator: ','
 //   }))
 //   .pipe(fs.createWriteStream('./data/population.json'));
-server.get('/api/get_aqi_beijing', function (req, res) {
-  res.json(aqiBeijingJson)
-})
 // server.get('/get_population', function (req, res) {
 //   res.json(populationJson)
 // })
--- src/apps/Index/StoryLine/index.js
+++ src/apps/Index/StoryLine/index.js
@@ -1,11 +1,33 @@
 import style from './style.less'
 import template from './template.html'
-
+import StoryLine from '../../../components/StoryLineView'
+import $ from 'jquery'
 export default{
   template,
   data () {
     return {
-      style
+      style,
+      teamCompeteInfo: null,
+      teamColor: null,
+      elId: `storyline-${(+new Date())}-${Math.random() * 100 * 1000 * 1000}`
     }
+  },
+  components: {
+    StoryLine
+  },
+  watch: {
+  },
+  methods: {
+    getData () {
+      $.getJSON('/api/get_team_compete_info', (data) => {
+        this.teamCompeteInfo = data
+      })
+      $.getJSON('/api/get_team_color', (data) => {
+        this.teamColor = data
+      })
+    }
+  },
+  ready () {
+    this.getData()
   }
 }
